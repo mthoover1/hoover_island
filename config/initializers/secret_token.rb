b4ac198e71ec7d0bbf25ae84d9c4a1be73a9d42f
Matthew Hoover <mthoover1@gmail.com>
2014-03-19 18:11:12 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HooverIsland::Application.config.secret_key_base = '8b206184ba68850800f353c40f6d0c8f723cc579cffdbfb5e538b87ce46f9ba00375d6eb640d4cd9f56941505b90b7d409a852ede7e377767780be8b08b2ed0b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HooverIsland::Application.config.session_store :cookie_store, key: '_hoover_island_session'
