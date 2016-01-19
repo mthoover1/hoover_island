0f7f1ad4cbfda11a148326c25116523db4994d23
Matthew Hoover <mthoover1@gmail.com>
2016-01-18 19:58:40 -0600

    rough version of new_season email

--- app/mailers/person_mailer.rb
+++ app/mailers/person_mailer.rb
@@ -3,7 +3,13 @@ class PersonMailer < ActionMailer::Base
 
   def forgot_password(account)
     @person = account.person
-    @url = calendar_path
+    @url = calendar_path #make this url eventually
     mail(to: account.email, subject: 'Hoover Island Password Reset')
+  end
+
+  def new_season(email)
+    @year = Date.today.year
+    @url = new_reservation_request_path #make this url eventually
+    mail(to: email, subject: "Now Accepting #{@year} Island Reservations")
   end
 end
