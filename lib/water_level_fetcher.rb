9171b0e743c250391ff7540b3d0fc7061e96c88e
Matthew Hoover <mthoover1@gmail.com>
2017-04-16 21:20:15 -0500

    update water_level_fetcher

--- lib/water_level_fetcher.rb
+++ lib/water_level_fetcher.rb
@@ -1,7 +1,7 @@
 require 'mechanize'
 
 class WaterLevelFetcher
-  STATION_ID = '05PB024'
+  URL = 'http://wateroffice.ec.gc.ca/report/real_time_e.html?stn=05PB024&mode=Table'
 
   def current_water_level
     UnitService.meters_to_feet(water_level_in_meters)
@@ -13,22 +13,15 @@ class WaterLevelFetcher
     most_recent_data_string.chomp('*').to_f
   end
 
-  def water_office_url
-    yesterday = Date.yesterday.strftime("%Y-%m-%d")
-    today = Date.today.strftime("%Y-%m-%d")
-
-    "http://wateroffice.ec.gc.ca/report/report_e.html?mode=Table&type=realTime&stn=#{STATION_ID}&dataType=&startDate=#{yesterday}&endDate=#{today}&prm1=46&prm2=47"
-  end
-
   def most_recent_data_string
     agent = Mechanize.new
 
-    agent.get(water_office_url) do |page|
-      form = agent.page.form_with(:action => "/include/disclaimer.php")
-      button = form.button_with(:value => "I Agree")
+    agent.get(URL) do |page|
+      form = agent.page.form_with(action: "/disclaimer_e.html")
+      button = form.button_with(value: "I Agree")
       data_page = agent.submit(form, button)
 
-      return data_page.search('table:first tr:last td:last').first.text
+      return data_page.search('table:first tbody tr:last td:last').first.text
     end
   end
 end
